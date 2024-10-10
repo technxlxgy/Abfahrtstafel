37aa11ce3eb839d886fbfe400eddfbae0ba2f356
isa03 <iva.savic@brueggli.ch>
2024-10-10 16:21:08 +0200

    fix: change data type number

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using RestSharp;
-using System.Reflection.Metadata.Ecma335;
 
 namespace Stationboard
 {
@@ -11,6 +10,9 @@ namespace Stationboard
 
             StationboardResponse response = GetStationboard();
 
+            //string headingFormat = String.Format("{0, -20} {1, -20} {2, -20} {3, -20}", "Departure Time", "Dealy in Minutes", "Vehicle", "Destination");
+            //Console.WriteLine(headingFormat);
+
             // iterates over every object in stationboard
             foreach (Stationboard sb in response.stationboard)
             {
@@ -18,8 +20,8 @@ namespace Stationboard
                 string delay = ExtractDelay(sb.stop.delay);
                 string vehicle = ExtractVehicle(sb);
 
-                // TODO Ausgabe formatieren (String.format)
-                Console.WriteLine("{0} {1} {2} {3}", departure, delay, vehicle, sb.to);
+                string strFormat = String.Format("{0} {1, -5} {2, -10} {3, -20}", departure, delay, vehicle, sb.to);
+                Console.WriteLine(strFormat);
             }
         }
 
@@ -28,20 +30,20 @@ namespace Stationboard
             var client = CreateClient();
             var request = CreateRequest();
             var response = client.Get<StationboardResponse>(request); // get json data
-            // Fehler abfangen wenn 'response' null
             return response;
         }
 
         private static RestClient CreateClient()
         {
+            // TODO Max timeout?
             var options = new RestClientOptions("https://transport.opendata.ch/v1");
             return new RestClient(options);
         }
 
         private static RestRequest CreateRequest() {
             RestRequest request = new RestRequest("stationboard");
-            request.AddParameter("station", "Zurich");
-            request.AddParameter("limit", 50);
+            request.AddParameter("station", "Zurich"); // TODO In Konstante speichern
+            request.AddParameter("limit", 50); // TODO In Konstante speichern
             request.AddParameter("fields[]", "stationboard/stop/departure");
             request.AddParameter("fields[]", "stationboard/stop/delay");
             request.AddParameter("fields[]", "stationboard/category");
@@ -57,7 +59,7 @@ namespace Stationboard
         // TODO Stimmt diese Methode?
         private static string ExtractDelay(int? delay) => delay == null || delay == 0 ? "" :  "+" + delay;
 
-        private record Stationboard(StationboardStop stop, string to, int number, string category); // stop => nested inside stationboard
+        private record Stationboard(StationboardStop stop, string to, string number, string category); // stop => nested inside stationboard
         private record StationboardStop(string departure, int? delay); // delay nullable value type
         private record StationboardResponse(Stationboard[] stationboard);
     }
