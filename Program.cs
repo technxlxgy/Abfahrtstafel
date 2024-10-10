ae63d56246b2bfff9e5100440a6f9f91b0c50c23
isa03 <iva.savic@brueggli.ch>
2024-10-10 12:55:34 +0200

    refactor: extract code to methods

--- Program.cs
+++ Program.cs
@@ -9,46 +9,53 @@ namespace Stationboard
         {
             Console.WriteLine("### Abfahrtstafel ###");
 
+            StationboardResponse response = GetStationboard();
+
+            // iterates over every object in stationboard
+            foreach (Stationboard sb in response.stationboard)
+            {
+                string departure = ExtractDepartureTime(sb);
+                string delay = ExtractDelay(sb.stop.delay);
+                string vehicle = ExtractVehicle(sb);
+
+                // TODO Ausgabe formatieren (String.format)
+                Console.WriteLine("{0} {1} {2} {3}", departure, delay, vehicle, sb.to);
+            }
+        }
+
+        private static StationboardResponse GetStationboard()
+        {
+            var client = CreateClient();
+            var request = CreateRequest();
+            var response = client.Get<StationboardResponse>(request); // get json data
+            // Fehler abfangen wenn 'response' null
+            return response;
+        }
+
+        private static RestClient CreateClient()
+        {
             var options = new RestClientOptions("https://transport.opendata.ch/v1");
-            var client = new RestClient(options);
-            var request = new RestRequest("stationboard");
-            request.AddParameter("station", "Romanshorn");
-            request.AddParameter("limit", 5);
+            return new RestClient(options);
+        }
+
+        private static RestRequest CreateRequest() {
+            RestRequest request = new RestRequest("stationboard");
+            request.AddParameter("station", "Zurich");
+            request.AddParameter("limit", 50);
             request.AddParameter("fields[]", "stationboard/stop/departure");
             request.AddParameter("fields[]", "stationboard/stop/delay");
             request.AddParameter("fields[]", "stationboard/category");
             request.AddParameter("fields[]", "stationboard/number");
             request.AddParameter("fields[]", "stationboard/to");
-
-            var response = client.Get<StationboardResponse>(request); // get json data
-
-            // iterates over every object in stationboard
-            foreach (Stationboard sb in response.stationboard) {              
-                DateTime dt = DateTime.Parse(sb.stop.departure);
-                
-                string departure = dt.ToString("HH:mm");
-                string delay = ExtractDelay(sb.stop.delay);
-                string vehicle = sb.category + sb.number;
-                string destination = sb.to;
-
-                Console.WriteLine("{0} {1} {2} {3}", departure, delay, vehicle, destination);
-            }
+            return request;
         }
 
-        // methode verbessern (einfacher, kürzer?)
+        private static string ExtractDepartureTime(Stationboard sb) => DateTime.Parse(sb.stop.departure).ToString("HH:mm");
+
+        private static string ExtractVehicle(Stationboard sb) => sb.category + sb.number;
+
+        // TODO Stimmt diese Methode?
         private static string ExtractDelay(int? delay) => delay == null || delay == 0 ? "" :  "+" + delay;
-        /*{
-            if (delay == null)
-            {
-                return "";
-            } else if (delay == 0)
-            {
-                return "";
-            } else
-            {
-                return "+" + delay;
-            }
-        }*/
 
         private record Stationboard(StationboardStop stop, string to, int number, string category); // stop => nested inside stationboard
         private record StationboardStop(string departure, int? delay); // delay nullable value type
