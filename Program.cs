2ed6a70d250bc467be6ebcf3303c107e2b4847c0
isa03 <iva.savic@brueggli.ch>
2024-10-11 11:31:51 +0200

    fix: error fixed

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ namespace Stationboard
 {
     class Program
     {
-        // default constant values (if no input is given)
         public const string DEST_PLACE = "Romanshorn";
         public const int LIMIT = 3;
 
@@ -12,54 +11,27 @@ namespace Stationboard
         {
             Console.WriteLine("### Abfahrtstafel ###");
 
-            // default station and limit
-            string inputDestPlace = DEST_PLACE;
-            int inputLimit = LIMIT;
- 
-            foreach(var arg in args)
+            StationboardResponse response = GetStationboard();
+
+            string headingFormat = String.Format("{0, -20} {1, -22} {2, -22} {3}", "Departure Time", "Delay in Minutes", "Vehicle", "Destination");
+            Console.WriteLine(headingFormat);
+
+            // iterates over every object in stationboard
+            foreach (Stationboard sb in response.stationboard)
             {
-                if (arg.StartsWith("--station="))
-                {
-                    inputDestPlace = arg.Substring("--station".Length);
-                }
-                else if (arg.StartsWith("--limit=="))
-                {
-                    if (int.TryParse(arg.Substring("--limit".Length), out int limit))
-                    { 
-                        inputLimit = limit;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid limit value. Using default limit.");
-                    }
-                }
+                string departure = ExtractDepartureTime(sb);
+                string delay = ExtractDelay(sb.stop.delay);
+                string vehicle = ExtractVehicle(sb);
+
+                string strFormat = String.Format("{0, -20} {1, -22} {2, -22} {3}", departure, delay, vehicle, sb.to);
+                Console.WriteLine(strFormat);
             }
-            //if (args.Length > 0)
-            //{
-                //inputDestPlace = args[0];
-                //inputLimit = int.Parse(args[1]);
-                StationboardResponse response = GetStationboard();
-
-                string headingFormat = String.Format("{0, -20} {1, -22} {2, -22} {3}", "Departure Time", "Delay in Minutes", "Vehicle", "Destination");
-                Console.WriteLine(headingFormat);
-
-                // iterates over every object in stationboard
-                foreach (Stationboard sb in response.stationboard)
-                {
-                    string departure = ExtractDepartureTime(sb);
-                    string delay = ExtractDelay(sb.stop.delay);
-                    string vehicle = ExtractVehicle(sb);
-
-                    string strFormat = String.Format("{0, -20} {1, -22} {2, -22} {3}", departure, delay, vehicle, sb.to);
-                    Console.WriteLine(strFormat);
-                }
-            //}
         }
 
-        public static StationboardResponse GetStationboard(string destPlace,  int limit)
+        public static StationboardResponse GetStationboard()
         {
             var client = CreateClient();
-            var requestStationboard = CreateRequestStationboard(destPlace, limit);
+            var requestStationboard = CreateRequestStationboard();
             var response = client.Get<StationboardResponse>(requestStationboard); // get json data
             return response;
         }
@@ -71,11 +43,11 @@ namespace Stationboard
             return new RestClient(options);
         }
 
-        public static RestRequest CreateRequestStationboard(string destPlace, int limit)
+        public static RestRequest CreateRequestStationboard()
         {
             RestRequest requestStationboard = new RestRequest("stationboard");
-            requestStationboard.AddParameter("station", destPlace);
-            requestStationboard.AddParameter("limit", limit);
+            requestStationboard.AddParameter("station", DEST_PLACE);
+            requestStationboard.AddParameter("limit", LIMIT);
             requestStationboard.AddParameter("fields[]", "stationboard/stop/departure");
             requestStationboard.AddParameter("fields[]", "stationboard/stop/delay");
             requestStationboard.AddParameter("fields[]", "stationboard/category");
@@ -89,7 +61,7 @@ namespace Stationboard
         public static string ExtractVehicle(Stationboard sb) => sb.category + sb.number;
 
         // TODO Stimmt diese Methode?
-        public static string ExtractDelay(int? delay) => delay == null || delay == 0 ? "" :  "+" + delay;
+        public static string ExtractDelay(int? delay) => delay == null || delay == 0 ? "" : "+" + delay;
 
         public record Stationboard(StationboardStop stop, string to, string number, string category); // stop => nested inside stationboard
         public record StationboardStop(string departure, int? delay); // delay nullable value type
